ddab6207d483165a6ac80bd68a78c76875feb1f4
James Duffield <duffers20@gmail.com>
2017-10-11 19:22:48 +0100

    Updated label in status bar.

--- src/app/status-bar/status-bar.component.ts
+++ src/app/status-bar/status-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-status-bar',
@@ -6,12 +7,24 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./status-bar.component.css']
 })
 export class StatusBarComponent implements OnInit {
-  @Input() countRemaining: number;
+  @Input() countRemaining: Observable<number>;
   @Output() removeCompleted: EventEmitter<any> = new EventEmitter();
+
+  public count: number;
+  public label: string;
 
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.countRemaining.subscribe ((count) => {
+      this.count = count;
+      if (this.count === 1) {
+        this.label = 'item';
+      } else {
+        this.label = 'items';
+      }
+    });
+  }
 
   public removeCompletedHandler() {
     this.removeCompleted.emit()
